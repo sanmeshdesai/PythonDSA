e0cb5a17e986919162e9121a91967502b63a062f
sd <saneshdesai11@gmail.com>
2025-12-24 11:44:18 +0530

    insertion sort

---
+++ dsa/InsertionSort.py
@@ -0,0 +1,21 @@
+nums = [3, 5, 6, 4, 8, 9, 10, 7, 1]
+
+def insertion_sort(nums):
+    n = len(nums)
+    for i in range(1, n):
+        key = nums[i]
+        j = i-1
+        while j >= 0 and nums[j] > key:
+            nums[j+1] = nums[j]
+            j -= 1
+
+        nums[j+1] = key
+
+    return nums
+
+print(insertion_sort(nums))
+
+
+
+
+
